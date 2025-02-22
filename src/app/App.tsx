3132ea2cc1feb3e0f1543e0a3e74d3e90041a559
KirillDolzhenkov <k.dolzhenkov@gmail.com>
2025-02-22 13:16:03 +0300

    init

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+          <div>
+              <input type="text"/>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
